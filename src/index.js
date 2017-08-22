68cacd8ad0f3007d9642510a5cd44891859537ad
Nick Johnson <nickjohnson.dev@gmail.com>
2017-08-22 16:44:46 -0500

    Basic webpack+webpack-dev-server setup

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import join from 'lodash/fp/join';
+import printMe from './printMe';
+import './styles/index.css';
+
+function component() {
+  const el = document.createElement('div');
+  const btn = document.createElement('button');
+
+  el.innerHTML = join(' ', ['Hello', 'Webpack']);
+
+  el.classList.add('hello');
+
+  btn.innerHTML = 'Click me and check the console!';
+
+  btn.onclick = printMe;
+
+  el.appendChild(btn);
+
+  return el;
+}
+
+document.body.appendChild(component());
